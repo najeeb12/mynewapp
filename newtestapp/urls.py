af40b6bda090b07dcbcef9bf8da6c3417e2e6807
Lycore Pvt Ltd - 3 <zaman@globalbridgeinc.net>
2016-11-10 22:40:18 +0500

    static url changes

--- newtestapp/urls.py
+++ newtestapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from newtestapp.views import *
+from newtestapp import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
